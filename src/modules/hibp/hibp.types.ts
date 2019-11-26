8576b37cacc17a8699c779fa195215e059820a47
anicodemo <alejandronicodemo@gmail.com>
2019-11-26 12:55:39 -0300

    0.1.65

--- src/modules/hibp/hibp.types.ts
+++ src/modules/hibp/hibp.types.ts
@@ -6,6 +6,7 @@ export interface HIBPUnknownBreaches { [bitPos: number]: LeakipediaSource };
 
 export class HIBPSearchResponseEntity {
   results!: { [email: string]: number[] }
+  strategies!: { [email: string]: string }
   unknownBreaches?: HIBPUnknownBreaches;
 }
 
@@ -14,4 +15,5 @@ export interface HIBPKnownBreaches { [name: string]: Bitpos };
 export class HIBPSearchDto {
   emails!: string[];
   knownBreaches?: HIBPKnownBreaches;
+  wait?: number;
 }
